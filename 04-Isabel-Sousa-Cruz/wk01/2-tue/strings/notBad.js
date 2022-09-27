cbcc6371787c37cbad16aaffc6c26cf75e737d49
isabelsousac <isabel.sousa15246@gmail.com>
2022-09-27 23:07:17 +1000

    delete comments from homework 27-09-2022

--- 04-Isabel-Sousa-Cruz/wk01/2-tue/strings/notBad.js
+++ 04-Isabel-Sousa-Cruz/wk01/2-tue/strings/notBad.js
@@ -1,16 +1,3 @@
-/*
-Create a function called notBad that takes a single argument, a string.
-
-It should find the first appearance of the substring 'not' and 'bad'.
-If the 'bad' follows the 'not', then it should replace the whole 'not'...'bad' substring with 'good' and return the result.
-If it doesn't find 'not' and 'bad' in the right sequence (or at all), just return the original sentence.
-For example:
-
-  notBad('This dinner is not that bad!'): 'This dinner is good!'
-  notBad('This movie is not so bad!'): 'This movie is good!'
-  notBad('This dinner is bad!'): 'This dinner is bad!'
-*/
-
 const notBad = function(str) {
     for (let i = 3; i < str.length; i++) {
         const current3LetterWord = str.substring(i - 3, i);
@@ -27,6 +14,6 @@ const notBad = function(str) {
     return str;
 }
 
-console.log(notBad("This dinner is not that bad! and blabla"));
+console.log(notBad("This dinner is not that bad!"));
 console.log(notBad("This movie is not so bad!"));
 console.log(notBad("This dinner is bad!"));
