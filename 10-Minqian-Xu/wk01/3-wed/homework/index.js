b2656e008a49769514e3a1875ea78e28cba75fb6
kyliexmq <114300308+kyliexmq@users.noreply.github.com>
2022-10-16 19:53:38 +1100

    homework

---
+++ 10-Minqian-Xu/wk01/3-wed/homework/index.js
@@ -0,0 +1,55 @@
+// Array and Functions Bonus Material
+// Define a function maxOfTwoNumbers that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in Javascript. You'll have to remember your pre-work, or do some googling to figure this out.
+
+const maxOfTwoNumbers = function (a,b){
+    if (a > b){
+        return a;
+    } else {
+        return b;
+    }
+
+}
+
+
+
+// Define a function maxOfThree that takes three numbers as arguments and returns the largest of them.
+const maxOfThree = function (a,b,c){
+    if (a > b && a > c){
+        return a;
+    } else if (b > a && b > c){
+        return b;
+    } else {
+        return c;
+    }
+}
+
+// Write a function that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
+
+const vowel = function (a){
+    let a.length = n;
+    if (a[n] = s | (a[n] = e && a[n-1] = s)){
+        return true;
+    } else {
+        return false;
+    }
+    
+
+
+}
+
+// Define a function sumArray and a function multiplyArray that sums and multiplies (respectively) all the numbers in an array of numbers. For example, sumArray([1,2,3,4]) should return 10, and multiplyArray([1,2,3,4]) should return 24.
+const sumArray = function (a){
+    let a.length = n;
+    sum = (1+n)*2;
+    return sum;
+}
+
+const multiplyArray = function (a){
+    let a.length = n;
+    multiply = n 
+
+
+// Bonus
+// Define a function reverseString that computes the reversal of a string. For example, reverseString("jag testar") should return the string "ratset gaj".
+// Write a function findLongestWord that takes an array of words and returns the length of the longest one.
+// Write a function filterLongWords that takes an array of words and an number i and returns the array of words that are longer than i.
