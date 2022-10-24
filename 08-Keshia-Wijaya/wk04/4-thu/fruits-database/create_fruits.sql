d1651e06171d4f2ced72ef40926ff11d5f457596
Keshia Wijaya <pawspower@Keshias-MacBook-Pro.local>
2022-10-24 11:19:41 +1100

    Kesh homework week04

---
+++ 08-Keshia-Wijaya/wk04/4-thu/fruits-database/create_fruits.sql
@@ -0,0 +1,11 @@
+CREATE TABLE fruits (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    family TEXT,
+    image TEXT 
+);
+
+
+INSERT INTO fruits (name, family) VALUES ('Watermelon', 'Cucurbits');
+INSERT INTO fruits (name, family) VALUES ('Mango', 'Anarcardie');
+INSERT INTO fruits (name, family) VALUES ('Papaya', 'Caricaceae');
