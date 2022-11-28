f9516dec0bab6d09cc38985622c89e12099fc237
Sophia Pache <sophpache@gmail.com>
2022-11-29 09:33:53 +1100

    homework

--- 11-Sophia-Pache/wk05/1-mon/games_on_rails/app/controllers/magicball_controller.rb
+++ 11-Sophia-Pache/wk05/1-mon/games_on_rails/app/controllers/magicball_controller.rb
@@ -1,5 +1,22 @@
 class MagicballController < ApplicationController
     def result
-        @array = ['As I see it, yes.', 'Very doubtful. '].sample
+        @array = [
+            "It is certain.",
+            "It is decidedly so.",
+            "Without a doubt.",
+            "Yes definitely.",
+            "You may rely on it",
+            "Signs point to yes.",
+            "Reply hazy, try again.",
+            "Ask again later.",
+            "Better not tell you now.",
+            "Cannot predict now.",
+            "Concentrate and ask again.",
+            "Don't count on it.",
+            "My reply is no.",
+            "My sources say no.",
+            "Outlook not so good",
+            "Very doubtful."
+        ].sample
     end
 end
---
+++ 11-Sophia-Pache/wk08/1-mon/dog-facts/src/components/Display.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Display = function (props) {
+    return (
+        <div>
+            { props.beers }
+        </div>
+    )
+}
+
+export default Display;
